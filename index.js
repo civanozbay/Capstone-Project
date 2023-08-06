5931fb67b754930c937ef1ebefe9c7b13eb082cb
civanozbay <civan417@hotmail.com>
2023-08-06 20:10:11 +0200

    html and css are completed as fundamental

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-app.use(express.static("views"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
