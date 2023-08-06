d93b92267080a35b3930ed61db79d78167885fd5
civanozbay <civan417@hotmail.com>
2023-08-06 18:14:59 +0200

    nodemon / index.ejs / some css added

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//asdasdad
+import express from "express";
+
+const app = express();
+
+app.use(express.static("views"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(3000, () => {
+  console.log("listening the port");
+});
