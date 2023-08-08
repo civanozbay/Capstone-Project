a9e3170742b91fd7c512554e68dfc8b91993a0cb
civanozbay <civan417@hotmail.com>
2023-08-08 09:51:29 +0200

    items array and logic for appearing items are added

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const items = ["Item 1", "Item 2"];
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { items: items });
+});
+
+app.post("/", (req, res) => {
+  const newItem = req.body.newItem;
+  items.push(newItem); // Yeni öğeyi items dizisine ekle
+  res.redirect("/"); // Anasayfaya yönlendirin
 });
 
 app.listen(3000, () => {
