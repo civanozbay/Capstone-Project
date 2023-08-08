827885deffe1a7f3c40d580fdc1adbf0c845967e
civanozbay <civan417@hotmail.com>
2023-08-08 14:43:08 +0200

    work page added

--- index.js
+++ index.js
@@ -6,16 +6,30 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var dt = Date();
+var day = dt.slice(0, 10);
+
 const items = ["Item 1", "Item 2"];
+const workItems = [];
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { items: items });
+  res.render("index.ejs", { items: items, dt: day });
 });
 
 app.post("/", (req, res) => {
   const newItem = req.body.newItem;
   items.push(newItem); // Yeni öğeyi items dizisine ekle
   res.redirect("/"); // Anasayfaya yönlendirin
+});
+
+app.get("/work", (req, res) => {
+  res.render("work.ejs", { workItems: workItems });
+});
+
+app.post("/work", (req, res) => {
+  const workItem = req.body.newItem;
+  if (workItem) workItems.push(workItem);
+  res.redirect("/work");
 });
 
 app.listen(3000, () => {
